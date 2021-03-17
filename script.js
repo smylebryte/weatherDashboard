04adb12a2960cbdff7f58d69b7543916665d5f16
Smylebryte <aliciahughes1446@gmail.com>
2021-03-17 02:28:14 -0500

    added functions for uvIndex and forecast

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var API_KEY = "70af00e0dc1696f9da44720427b65955"
 
+
 $(document).ready(function() {
     $("#search-button").on("click", function() {
         let city=$("#search-value").val();
@@ -14,24 +15,40 @@ $(document).ready(function() {
             success: function(data) {
                 console.log(data);
                 $("#tw-city_name").text(data["name"]);
-                $("#tw-temp").text("Temperature:" + " " + data["main"]["temp"]);
-                $("#tw-humidity").text("Humidity:" + " " + data["main"]["humidity"]);
+                $("#tw-temp").text("Temperature:" + " " + data["main"]["temp"] + " " + "°F");
+                $("#tw-humidity").text("Humidity:" + " " + data["main"]["humidity"] + "%");
                 $("#tw-wind_speed").text("Wind Speed:" + " " + data["wind"]["speed"] + " " + "MPH");
+
+                let lat=data["coord"]["lat"];
+                let lon=data["coord"]["lon"];
+
+                uvIndex(lat, lon);
+                forecastCityWeather(city);
             }
         })
     }
+});
 
+function forecastCityWeather(city) {
+    $.ajax({
+        type: "Get",
+        url: `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`,
+        datatype: "json",
+        success: function(data) {
+            console.log(data);
+            
+        }
+    })
+}
 
-//     function uvIndex(lat, lon) {
-//         $.ajax({
-//             type: "Get",
-//             url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`,
-//             datatype: "json",
-//             success: function(data) {
-//                 console.log(data);
-//                 $("#tw-wind_speed").text("Wind Speed:" + " " + data["wind"]["speed"] + " " + "MPH");
-//             }
-//         })
-
-//     }
-// });
+function uvIndex(lat, lon) {
+    $.ajax({
+        type: "Get",
+        url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`,
+        datatype: "json",
+        success: function(data) {
+            console.log(data);
+            $("#tw-uv_index").text("UV Index:" + " " + data["current"]["uvi"]);
+        }
+    })
+}
